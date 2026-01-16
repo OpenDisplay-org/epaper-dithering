4e2e574dbc6119f0c954ef9b03480ef31cc4bc7f
gabriel <admin@g4bri3l.de>
2026-01-16 22:29:23 +0100

    fix: update BWGBRY color palette order

--- packages/javascript/src/palettes.ts
+++ packages/javascript/src/palettes.ts
@@ -50,10 +50,10 @@ const PALETTES: Record<ColorScheme, ColorPalette> = {
     colors: {
       black: { r: 0, g: 0, b: 0 },
       white: { r: 255, g: 255, b: 255 },
-      green: { r: 0, g: 255, b: 0 },
-      blue: { r: 0, g: 0, b: 255 },
-      red: { r: 255, g: 0, b: 0 },
       yellow: { r: 255, g: 255, b: 0 },
+      red: { r: 255, g: 0, b: 0 },
+      blue: { r: 0, g: 0, b: 255 },
+      green: { r: 0, g: 255, b: 0 },
     },
     accent: 'red',
   },
